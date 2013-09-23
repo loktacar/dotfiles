56ba394ad69c4585b25a3ea63bdf6d6ad8788ac4
Viktor Alex <viktoralex@365.is>
2013-09-23 10:54:18 +0000

    Turned off pager in psqlrc because I use tmux, install now makes the vimproc plugin and some minor changes and merge to tmux-workon script

--- vim/install.sh
+++ vim/install.sh
@@ -8,4 +8,11 @@ ln -s $1/vim ~/.vim
 rm -f ~/.vimrc
 ln -s $1/vimrc ~/.vimrc
 
+# Make shougo-vimproc
+if [ -f $1/vim/bundle/shugo-vimproc/Makefile ];
+then
+  cd $1/vim/bundle/shugo-vimproc
+  make > /dev/null
+fi
+
 echo "done."
