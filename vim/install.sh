2ac973093f22f60ab15d905a2319ac22467272c3
Viktor Alex <viktoralex@365.is>
2015-02-26 19:00:24 +0000

    Added Vundle installation to install script for vim, it's no longer a submodule.

--- vim/install.sh
+++ vim/install.sh
@@ -8,11 +8,20 @@ ln -s $1/vim ~/.vim
 rm -f ~/.vimrc
 ln -s $1/vimrc ~/.vimrc
 
+# Install Vundle.vim
+# only installed if the directory doesn't exist, it's updated via BundleUpdate (see next command)
+if [ ! -d $1/vim/bundle/Vundle.vim ]; then
+  git clone https://github.com/gmarik/Vundle.vim.git $1/vim/bundle/Vundle.vim > /dev/null
+fi
+
+# Install and or update Vundle plugins
+vim -c "BundleInstall" -c "BundleUpdate" -c "qall"
+
 # Make shougo-vimproc
-if [ -f $1/vim/bundle/vimproc.vim/Makefile ];
-then
+if [ -f $1/vim/bundle/vimproc.vim/Makefile ]; then
   cd $1/vim/bundle/vimproc.vim
   make > /dev/null
 fi
 
 echo "done."
+
