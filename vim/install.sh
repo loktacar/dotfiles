b324105c0072b5ce1795c20f1e73a0b65b58bff9
Viktor Alex <viktoralex@365.is>
2015-08-26 10:04:48 +0000

    Changed plugin manager from Vundle to plug.vim

--- vim/install.sh
+++ vim/install.sh
@@ -25,21 +25,17 @@ ln -s $location/vim ~/.vim
 rm -f ~/.vimrc
 ln -s $location/vimrc ~/.vimrc
 
-# Install Vundle.vim
+# Install plug.vim plugin manager
 # only installed if the directory doesn't exist, it's updated via BundleUpdate (see next command)
-if [ ! -d $location/vim/bundle/Vundle.vim ]; then
-  git clone --quiet https://github.com/gmarik/Vundle.vim.git $location/vim/bundle/Vundle.vim
+if [ ! -d $location/autoload/plug.vim ]; then
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 # Install and or update Vundle plugins
 if [ ! $SKIP_VIM_PLUGINS ]; then
-  vim -c "BundleInstall" -c "BundleUpdate" -c "qall"
-fi
-
-# Make shougo-vimproc
-if [ -f $location/vim/bundle/vimproc.vim/Makefile ]; then
-  cd $location/vim/bundle/vimproc.vim
-  make > /dev/null
+  #vim -c "BundleInstall" -c "BundleUpdate" -c "qall"
+  vim -c "PlugInstall" -c "qall"
 fi
 
 echo "done."
