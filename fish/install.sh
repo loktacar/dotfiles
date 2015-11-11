d6d2876b0c69aee7d52da4b339e0c9b9d5cea5ee
Viktor Alex <viktoralex@365.is>
2015-11-11 22:30:21 +0000

    Removing force flag from remove (rm) in install scripts

--- fish/install.sh
+++ fish/install.sh
@@ -6,8 +6,8 @@ location="${@: -1}"
 
 mkdir -p ~/.config/fish
 
-rm -f ~/.config/fish/config.fish
-rm -f ~/.config/fish/functions
+rm ~/.config/fish/config.fish
+rm ~/.config/fish/functions
 ln -s $location/config.fish ~/.config/fish/config.fish
 ln -s $location/functions ~/.config/fish/functions
 
