47084b3d824586e52da8cc42126cc75f658d6566
Viktor Alex <viktoralex@365.is>
2015-11-17 11:14:46 +0000

    Move tmux alias to functions folder

--- fish/config.fish
+++ fish/config.fish
@@ -4,8 +4,3 @@
 # Supress greeting
 function fish_greeting
 end
-
-# 256 color in tmux
-function tmux
-  env TERM=xterm-256color tmux $argv
-end
---
+++ fish/functions/tmux.fish
@@ -0,0 +1,4 @@
+# 256 color in tmux
+function tmux
+  env TERM=xterm-256color tmux $argv
+end
