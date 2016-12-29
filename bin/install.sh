1c902d81b12289683ee2b9776c8c25129b13bb77
Viktor Alex <viktoralex@365.is>
2016-12-29 10:41:45 +0000

    Reactivate installation script

--- bin/install.sh
+++ bin/install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-#echo -n "Installing bin files... "
+echo -n "Installing bin files... "
 
-#eval binfolder="~/.bin"
+eval binfolder="~/.bin"
 
-#mkdir -p "$binfolder"
+mkdir -p "$binfolder"
 
-#for location in "${@: -1}"/*; do
-#  filename="$(basename "$location")"
-#  if [[ -f "$location" && -x "$location" && ! $filename == "install.sh" ]]; then
-#    rm "$binfolder/$filename" 2> /dev/null
-#    ln -s "$location" "$binfolder"/"$filename"
-#  fi
-#done
+for location in "${@: -1}"/*; do
+  filename="$(basename "$location")"
+  if [[ -f "$location" && -x "$location" && ! $filename == "install.sh" ]]; then
+    rm "$binfolder/$filename" 2> /dev/null
+    ln -s "$location" "$binfolder"/"$filename"
+  fi
+done
 
-#echo "done."
+echo "done."
