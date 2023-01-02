d225c0ec828310cd91115820c4f4815aa546d4f5
Viktor Alex <viktoralex@vodafone.is>
2023-01-02 10:40:42 +0000

    Have plug.vim auto-install on startup

--- vim/install.sh
+++ vim/install.sh
@@ -31,18 +31,5 @@ ln -s $location/vim ~/.config/nvim
 rm -f ~/.config/nvim/init.vim 2> /dev/null
 ln -s $location/vimrc ~/.config/nvim/init.vim
 
-# Install plug.vim plugin manager
-# only installed if the directory doesn't exist, it's updated via BundleUpdate (see next command)
-if [ ! -d $location/autoload/plug.vim ]; then
-  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
-
-# Install and or update Vundle plugins
-if [ ! $SKIP_VIM_PLUGINS ]; then
-  #vim -c "BundleInstall" -c "BundleUpdate" -c "qall"
-  vim -c "PlugInstall" -c "qall"
-fi
-
 echo "done."
 
